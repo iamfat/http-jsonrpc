510dc5ac651d85c4cb7475177687fc0fdb672599
Jia Huang <iamfat@moutai.local>
2014-05-15 03:43:25 +0800

    add later binding

--- README.md
+++ README.md
@@ -7,15 +7,20 @@ Examples
 ========
 
 ````javascript
-var rpc = require('http-jsonrpc');
+var RPC = require('http-jsonrpc');
 
-var client = rpc.connect('http://path/to/rpc', {q:"xxx"});
+var client = RPC.connect('http://path/to/rpc', {q:"xxx"});
 
 client
 .call('method', [p1,p2,p3])
 .done(function(ret){
 }, function(err){
 });
+
+// or later connect
+var client = new RPC();
+client.call('method', [p1,p2,p3]).done(function(ret){}, function(err){});
+client.connect('http://path/to/rpc', {q:"xxx"});
 
 var server = rpc.server();
 server.process(request);
--- test/rpc.js
+++ test/rpc.js
@@ -50,3 +50,50 @@ describe("JSON RPC over HTTP:", function(){
     })
 
 });
+
+
+describe("Later binding", function(){
+
+    var server = new httpRPC();
+    
+    var http = require('http');
+    http.createServer(function(req, res){
+        server.process(req, res);
+    }).listen(8088);
+
+    var client = new httpRPC();
+    
+    it("should be called", function(done) {
+
+        server.calling("foo", function(params) {
+            assert.equal(params.foo, "bar");
+            return params.foo;
+        });
+
+        client
+        .call("foo", {foo:"bar"})
+        .done(function(ret){
+            assert.equal(ret, "bar");
+            done();
+        }, done);
+
+    })
+
+    it("should catch Exception", function(done) {
+
+        server.calling("foo", function(params) {
+            throw new server.Exception("mocha", 9628);
+        });
+
+        client
+        .call("foo", {foo:"bar"})
+        .done(done, function(err) {
+            assert.equal(err.message, "mocha");
+            assert.equal(err.code, 9628);
+            done();
+        });
+
+    })
+
+    client.connect('http://localhost:8088/api');
+});
